4001a78d8029b765a5da327df1a655596de6e91a
andri2203 <randrip.22@gmail.com>
2023-03-03 21:54:40 +0700

    Tambah Tanda route tampil gambar

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
 
+// Route tampil gambar
 Route::get('/files/{target}/{image}', [FileHandleController::class, 'index']);
 
 Route::get('/download/{id}', [DownloadFileController::class, 'index']);
